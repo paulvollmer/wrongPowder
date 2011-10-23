e095339c9e623f6abd4c286006ee967eb1c0ad15
Paul Vollmer <wrongentertainment@yahoo.de>
2011-10-23 16:43:13 +0200

    Added BASE64 en/decoder class to Util packet

---
+++ processing/src/wrongPowder/util/BASE64.java
@@ -0,0 +1,82 @@
+/**
+ *  wrongPowder is developed by wrong-entertainment & powder
+ *
+ *
+ *  Copyright 2011 Paul Vollmer & Tim Pulver
+ *  paulvollmer.net
+ *  vollmerpaul@yahoo.de
+ *  timpulver.de
+ *  pulver.tim@googlemail.com
+ * 
+ *  This file is part of wrongPowder library.
+ *
+ *  wrongPowder is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with wrongPowder. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *  @author		##author##
+ *  @modified	##date##
+ *  @version	##version##
+ */
+
+package wrongPowder.util;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+
+
+/**
+ * BASE64
+ * 
+ * @example util_basic 
+ */
+public class BASE64 {	
+	
+
+	/**
+	 * A Constructor, usually called in the setup() method in your sketch to
+	 * initialize and start the library.
+	 * 
+	 * @example util_basic
+	 */
+	public BASE64() {}
+	
+	
+	/**
+	 * Encode a string with BASE64.
+	 * 
+	 * @param input
+	 * @param format
+	 * @return The encoded string.
+	 * @throws UnsupportedEncodingException
+	 * @throws IOException
+	 */
+	public String encode(String input, String format) throws UnsupportedEncodingException {
+		String encoded = new BASE64Encoder().encode(input.getBytes(format));
+	    return encoded; 
+	}
+	
+	
+	/**
+	 * Decode a String with BASE64.
+	 * 
+	 * @param input
+	 * @param format
+	 * @return The decoded string.
+	 * @throws UnsupportedEncodingException
+	 * @throws IOException
+	 */
+	public String decode(String input, String format) throws UnsupportedEncodingException, IOException {
+		String decoded = new String(new BASE64Decoder().decodeBuffer(input),format);
+		return decoded;
+	}
+ 	
+}
