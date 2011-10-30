e3568de15ef1a040e4365788d27867b28adc72a4
Paul Vollmer <wrongentertainment@yahoo.de>
2011-10-30 15:27:29 +0100

    Create empty Log class

---
+++ processing/src/wrongPowder/io/Log.java
@@ -0,0 +1,54 @@
+/**
+ *  wrongPowder is developed by wrong-entertainment & powder
+ *
+ *
+ *  Copyright 2011 Paul Vollmer & Tim Pulver
+ *  paulvollmer.net
+ *  vollmerpaul@yahoo.de
+ *  timpulver.de
+ *  pulver.tim@googlemail.com
+ * 
+ *  This file is part of wrongPowder library.
+ *
+ *  wrongPowder is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with wrongPowder. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *  @author		##author##
+ *  @modified	##date##
+ *  @version	##version##
+ */
+
+package wrongPowder.io;
+
+
+
+import java.io.BufferedWriter;
+
+
+/**
+ * Config
+ * 
+ * @example config_basic 
+ */
+public class Log {
+
+	//public 
+	
+	
+
+	/**
+	 * A Constructor, usually called in the setup() method in your sketch to
+	 * initialize and start the library.
+	 * 
+	 * @example config_basic
+	 */
+	public Log() {}
+	
+	
+	
+}
