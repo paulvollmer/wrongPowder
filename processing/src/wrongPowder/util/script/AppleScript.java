97bbd9c0d530a8f0f70539cdbab6a0d6eb0ae2e4
Paul Vollmer <wrongentertainment@yahoo.de>
2011-11-09 12:10:22 +0100

    delete unused java.io.File classes

--- processing/src/wrongPowder/util/script/AppleScript.java
+++ processing/src/wrongPowder/util/script/AppleScript.java
@@ -30,10 +30,6 @@ import javax.script.ScriptEngineFactory;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-
 import wrongPowder.io.Log;
 
 
