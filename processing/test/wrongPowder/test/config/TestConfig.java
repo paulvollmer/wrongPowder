178456def9ff0e39560e04d1d21739bea33d69f8
Paul Vollmer <wrongentertainment@yahoo.de>
2011-11-20 14:35:41 +0100

    Added JUnit testConfig class

---
+++ processing/test/wrongPowder/test/config/TestConfig.java
@@ -0,0 +1,118 @@
+package wrongPowder.test.config;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import wrongPowder.io.Config;
+
+public class TestConfig {
+
+	@Test
+	public void testConfig() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConfigStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoad() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoadStaticString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoadStaticStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStoreString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSize() {
+		Config config = new Config();
+		System.out.println("TEST config.getSize() = "+config.getSize());
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIntPropertyStringInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIntPropertyString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFloatPropertyStringFloat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFloatPropertyString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStringPropertyStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStringPropertyString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBooleanPropertyStringBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBooleanPropertyString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPropertyStringInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPropertyStringFloat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPropertyStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPropertyStringBoolean() {
+		fail("Not yet implemented");
+	}
+
+}
